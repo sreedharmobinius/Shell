89854d992396429d53bbf9fd66d0e7489778526a
manoj.v <manoj.v@mobinius.com>
2020-05-20 18:37:29 +0530

    Home screen static Ui implementation.

---
+++ dev-Shell-Smart-Ticketing-Retailer/CustomCell/HomeTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  HomeTableViewCell.swift
+//  dev-Shell-Smart-Ticketing-Retailer
+//
+//  Created by Manoj on 19/05/20.
+//  Copyright © 2020 Mobinius. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var lblStationName: UILabel!
+    @IBOutlet weak var lblLocation: UILabel!
+    @IBOutlet weak var lblInProgressCount: UILabel!
+    @IBOutlet weak var lblFeedBackCount: UILabel!
+    @IBOutlet weak var lblClosedCount: UILabel!
+    @IBOutlet weak var lblFeedBackBadge: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+//        self.lblFeedBackBadge.layer.cornerRadius = self.lblFeedBackBadge.frame.width/2
+        self.lblFeedBackBadge.layer.cornerRadius = 5
+
+        self.lblFeedBackBadge.layer.masksToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    
+    
+}
---
+++ dev-Shell-Smart-Ticketing-Retailer/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,82 @@
+//
+//  HomeViewController.swift
+//  dev-Shell-Smart-Ticketing-Retailer
+//
+//  Created by Manoj on 19/05/20.
+//  Copyright © 2020 Mobinius. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDelegate,UITableViewDataSource,UIActionSheetDelegate {
+   
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblStationCount: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var searchView: UIView!
+    
+    let cellReuseIdentifier = "cell"
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "HomeTableViewCell", bundle: nil), forCellReuseIdentifier: cellReuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           5
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell:HomeTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! HomeTableViewCell
+
+           cell.layer.borderWidth = 1
+           cell.layer.borderColor = UIColor.lightGray.cgColor
+           cell.layer.cornerRadius = 10
+           cell.clipsToBounds = true
+           return cell
+       }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 144
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let settingsActionSheet: UIAlertController = UIAlertController(title:nil, message:nil, preferredStyle:UIAlertController.Style.actionSheet)
+        settingsActionSheet.view.tintColor = UIColor.black
+        if let firstSubview = settingsActionSheet.view.subviews.first, let alertContentView = firstSubview.subviews.first {
+            for view in alertContentView.subviews {
+                view.backgroundColor = UIColor(red: 252, green: 207, blue: 0, alpha: 1)
+            }
+        }
+        
+
+        settingsActionSheet.addAction(UIAlertAction(title:"View Station Details         >", style:UIAlertAction.Style.default, handler:{ action in
+        }))
+        settingsActionSheet.addAction(UIAlertAction(title:"Cancel", style:UIAlertAction.Style.cancel, handler:nil))
+        present(settingsActionSheet, animated:true, completion:nil)
+    }
+    
+   
+    
+    @IBAction func SearchButtonClicked(_ sender: Any) {
+        self.searchView.isHidden = false
+    }
+    
+    @IBAction func EmergencyButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func NotificationButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func SearchBarBackClicked(_ sender: Any) {
+        self.searchView.isHidden = true
+    }
+}
